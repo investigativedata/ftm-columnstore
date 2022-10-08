fbfa069e5a6d9d39d5a41e91b2e675498e143055
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-10-08 17:18:32 +0200

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="ftm-columnstore",
-    version="0.0.4",
+    version="0.0.5",
     description="Column store implementation for ftm data based on clickhouse",
     long_description=long_description,
     long_description_content_type="text/markdown",
