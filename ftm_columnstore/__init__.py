020ffc866f7f1cd6a1406c56eedad8dcfe2070de
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-10-29 02:13:21 +0200

    Finally numpy is silenced

--- ftm_columnstore/__init__.py
+++ ftm_columnstore/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import warnings
 
+import numpy as np
+
 from .dataset import Dataset, get_dataset  # noqa
 from .driver import get_driver  # noqa
 
@@ -8,3 +10,4 @@ from .driver import get_driver  # noqa
 logging.getLogger("clickhouse_driver.columns.service").setLevel(logging.ERROR)
 logging.getLogger("numpy.core.fromnumeric").setLevel(logging.ERROR)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
