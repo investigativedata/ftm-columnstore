652746441a217a0adf7192bb0ba960fbecacdc2a
Simon Wörpel <simon.woerpel@medienrevolte.de>
2023-01-04 20:20:36 +0100

    switch to setup.cfg

--- ftm_columnstore/__init__.py
+++ ftm_columnstore/__init__.py
@@ -6,7 +6,7 @@ import numpy as np
 from .dataset import Dataset, get_dataset  # noqa
 from .driver import get_driver  # noqa
 
-# don't show numpy depcreaction warnings:
+# shut up
 logging.getLogger("clickhouse_driver.columns.service").setLevel(logging.ERROR)
 logging.getLogger("numpy.core.fromnumeric").setLevel(logging.ERROR)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
