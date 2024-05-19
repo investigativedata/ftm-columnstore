e5ce7cf65ae47cb0972639b1dc20d1d07db74a72
Simon Wörpel <simon.woerpel@pm.me>
2024-05-19 03:36:34 +0200

    🧪 sync with ftmq store tests

--- tests/test_fingerprints.py
+++ tests/test_fingerprints.py
@@ -1,5 +1,6 @@
 def test_fingerprints():
     # find similarities by phonetic algorithm
+    # FIXME query here for reference
     q = """SELECT entity_id FROM ftm_columnstore_test_fpx
         WHERE algorithm = '{algorithm}' AND value IN (
         SELECT value FROM (
@@ -11,20 +12,3 @@ def test_fingerprints():
             GROUP BY value
             HAVING entities > 2
         ))"""
-    # ds = get_dataset("luanda_leaks")
-    # ds = ds.store
-    # entities = [
-    #     e for e in ds.EQ.where(entity_id__in=q.format(algorithm="fingerprint"))
-    # ]
-    # self.assertEqual(len(entities), 125)
-    # FIXME
-    # names = [n for e in entities for n in e.names]
-    # for name in [
-    #     "Galp Energia Overseas Block 14 B.V.",
-    #     "Galp Energia Overseas Block 32 B.V.",
-    #     "Galp Energia Overseas Block 33 B.V.",
-    #     "Windhoek PEL 23 B.V.",
-    #     "Windhoek PEL 24 B.V.",
-    #     "Windhoek PEL 28 B.V.",
-    # ]:
-    #     self.assertIn(name, names)
