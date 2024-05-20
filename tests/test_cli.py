73f2c899c75ff5b679d93b3a5718e4cd411eeaf4
Simon Wörpel <simon.woerpel@pm.me>
2024-05-20 19:34:22 +0200

    ✨ (cli) iterate

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import orjson
-from nomenklatura.entity import CompositeEntity
 from typer.testing import CliRunner
 
 from ftm_columnstore.cli import cli
@@ -21,3 +19,16 @@ def test_cli(fixtures_path: Path):
     in_uri = str(fixtures_path / "eu_authorities.ftm.json")
     result = runner.invoke(cli, ["write", "-i", in_uri, "-d", "eu_authorities"])
     assert result.exit_code == 0
+
+    in_uri = str(fixtures_path / "donations.ijson")
+    result = runner.invoke(cli, ["write", "-i", in_uri, "-d", "donations"])
+    assert result.exit_code == 0
+
+    res = runner.invoke(cli, ["iterate"])
+    assert result.exit_code == 0
+    lines = _get_lines(res.stdout)
+    assert len(lines) == 625
+    res = runner.invoke(cli, ["iterate", "-d", "donations"])
+    assert result.exit_code == 0
+    lines = _get_lines(res.stdout)
+    assert len(lines) == 474
